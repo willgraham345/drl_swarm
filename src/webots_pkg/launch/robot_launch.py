3d4f12bc8af6ff8356b3db3a601790417754020b
Will <willgraham345@gmail.com>
2023-04-01 17:59:53 -0600

    Added tb to world, started working through crazychoir
    Look more into the crazyflie urdf they have, along with the crazychoir examples formation executable. Those will be where we can make some big progress on moving forward. Also look into executables for turtlebot motion.

--- src/webots_pkg/launch/robot_launch.py
+++ src/webots_pkg/launch/robot_launch.py
@@ -6,7 +6,7 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 from webots_ros2_driver.webots_launcher import WebotsLauncher
 from webots_ros2_driver.webots_launcher import Ros2SupervisorLauncher
-from webots_ros2_driver.utils import controller_url_prefix
+# from webots_ros2_driver.utils import controller_url_prefix
 from urdf_parser_py.urdf import URDF
 
 package_dir = get_package_share_directory('webots_pkg')
@@ -24,6 +24,23 @@ tb1 = swarm_module.Turtlebot('tb1', 'ROS2_address', [2, 2, 0])
 swarm = swarm_module.Swarm([tb1], [cf1, cf2])
 # print(swarm.crazyflies['cf1'].URI_address)
 
+def get_cf_driver(cf_name):
+    launch_description = []
+    robot_description = pathlib.Path(os.path.join(package_dir, 'crazyflie_fpqr.urdf')).read_text()
+
+    crazyflie_driver = Node(
+        package = 'webots_ros2_driver',
+        executable = 'driver',
+        namespace = cf_name,
+        output = 'screen',
+        additional_env = {
+            'WEBOTS_CONTROLLER_URL': cf_name,
+        },
+        parameters = [
+            {'robot_description': robot_description},
+        ]
+    )
+
 def generate_launch_description():
     webots_package_dir = get_package_share_directory('webots_pkg')
 
@@ -32,82 +49,27 @@ def generate_launch_description():
     )
     # I don't think the ros2 supervisor can come before the webots launcher. I'm not sure though.
     ros2_supervisor = Ros2SupervisorLauncher()
-    # urdf = os.path.join(webots_package_dir, 'resource', 'Crazyflie1.urdf')
-    # with open(urdf, 'r+') as infp:
-    #     urdf = URDF.from_xml_string(infp.read())
-    # print(urdf)
-    # Don't modify above this line, it'll screw stuff up
-    
-    # I don't think the state publishers acutally do anything. I think everything runs through the driver. I'm not sure though.
-    robot_description = pathlib.Path(os.path.join(package_dir, 'resource', 'Crazyflie1.urdf')).read_text()
-    print("robot_description: ", robot_description)
-    # cf1_state_publisher = Node(
-    #     package='robot_state_publisher',
-    #     executable='robot_state_publisher',
-    #     output='screen',
-    #     parameters=[{
-    #         'robot_description': '<robot name="Crazyflie1"><link name=""/></robot>',
-    #     }],
-    # )
-    cf1_driver = Node(
-        package='webots_ros2_driver',
-        executable='driver',
-        output='screen',
-        additional_env={'WEBOTS_CONTROLLER_URL': 'Crazyflie1'},
-        parameters=[{
-            'robot_description': robot_description,
-            'use_sim_time': True,
-            'set_robot_state_publisher': True,
-            'name': 'cf1',
-        }],
-    )
-    robot_description = pathlib.Path(os.path.join(package_dir, 'resource', 'Crazyflie2.urdf')).read_text()
-    # cf2_state_publisher = Node(
-    #     package='robot_state_publisher',
-    #     executable='robot_state_publisher',
-    #     output='screen',
-    #     namespace='cf2',
-    #     parameters=[{
-    #         'robot_description': robot_description,
-    #     }],
-    # )
-    cf2_driver = Node(
-        package='webots_ros2_driver',
-        executable='driver',
-        output='screen',
-        additional_env={'WEBOTS_CONTROLLER_URL': 'Crazyflie2'},
-        parameters=[{
-            'robot_description': robot_description,
-        }],
-    )
-
-    # my_robot_driver = Node(
-    #     package='webots_ros2_driver',
-    #     executable='driver',
-    #     output='screen',
-    #     # namespace='cf1_driver',
-    #     additional_env={'WEBOTS_CONTROLLER_URL': 'Crazyflie'},
-    #     parameters=[{
-    #         'robot_description': robot_description,
-    #         'use_sim_time': True,
-    #         'set_robot_state_publisher': True,
-    #     }],
-    # )
-    return LaunchDescription([
-        webots,
-        cf1_driver,
-        ros2_supervisor,
-        cf2_driver,
-        # cf1_state_publisher,
-        # robot_state_publisher2,
-        # my_robot_driver2,
-        launch.actions.RegisterEventHandler(
+    event_handler = launch.actions.RegisterEventHandler(
             event_handler=launch.event_handlers.OnProcessExit(
                 target_action=webots,
                 on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
             )
         )
-    ])
+    launch_description = [webots, ros2_supervisor, event_handler]
+    # The for loop is working, printing the strings correctly
+    for cf in swarm.crazyflies: 
+        launch_description.append(get_cf_driver(cf.name))
+        launch_description.append(Node(
+            package = 'robot_state_publisher',
+            executable = 'robot_state_publisher',
+            additional_env = {'WEBOTS_ROBOT_NAME': cf.name},
+            namespace=cf.name,
+            output = 'screen',
+            parameters = [
+                {'robot_description': '<robot name=""><link name=""/></robot>',}
+                ]
+            ))
 
+    return LaunchDescription(launch_description)
 if __name__ == "__main__":
     generate_launch_description()
