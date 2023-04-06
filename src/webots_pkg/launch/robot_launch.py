ddaf6e4327c47bd00c00462c9483d0bca729f0a1
Will <willgraham345@gmail.com>
2023-04-06 15:20:32 -0600

    Added tb files, beginning to impement

--- src/webots_pkg/launch/robot_launch.py
+++ src/webots_pkg/launch/robot_launch.py
@@ -2,89 +2,83 @@ import os
 import pathlib
 import launch
 from launch import LaunchDescription
-from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
-from webots_ros2_driver.webots_launcher import WebotsLauncher, Ros2SupervisorLauncher
+from webots_ros2_driver.webots_launcher import WebotsLauncher
+from webots_ros2_driver.webots_launcher import Ros2SupervisorLauncher
+from launch_ros.actions import LoadComposableNodes, Node
 
-# from webots_ros2_driver.utils import controller_url_prefix
-from urdf_parser_py.urdf import URDF
+# import importlib.util
+# # specify the full path of the file containing the class definition
+# current_file_path= os.getcwd()
+# directory_path = os.path.dirname(current_file_path)
+# swarm_path = os.path.join(directory_path, 'resource')
+# sys.path.insert(0, directory_path)
+# print("Current File Path: ", current_file_path)
+# print("Directory Path: ", os.path.dirname(current_file_path))
+# print("Swarm Path: ", swarm_path)
 
+# spec = importlib.util.spec_from_file_location("swarm_classes", os.path.join(swarm_path, 'swarm_classes.py'))
+# swarm_classes = importlib.util.module_from_spec(spec)
+# print("Swarm Classes: ", swarm_classes)
+# from resource import * # This imports swarm classes to the file
+# import swarm_classes
 package_dir = get_package_share_directory('webots_pkg')
 swarm_classes = pathlib.Path(os.path.join(package_dir, 'resource', 'swarm_classes.py'))
-import importlib.util
+print("Swarm Classes: ", swarm_classes)
+import importlib.utildw
 spec = importlib.util.spec_from_file_location("swarm_classes", swarm_classes)
 swarm_module = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(swarm_module)
 
 
 cf1 = swarm_module.Crazyflie('cf1', 'radio://0/80/2M/E7E7E7E7E7', [0, 0, 0])
-# cf2 = swarm_module.Crazyflie('cf2', 'radio://0/80/2M/E7E7E7E7E8', [1, 1, 0])
+cf2 = swarm_module.Crazyflie('cf2', 'radio://0/80/2M/E7E7E7E7E8', [1, 1, 0])
 
-tb1 = swarm_module.Turtlebot('tb1', 'ROS2_address', [-1, -1, 0])
-tb2 = swarm_module.Turtlebot('tb2', 'ROS2_address', [-2, -2, 0])
-swarm = swarm_module.Swarm([tb1, tb2], [cf1])
+tb1 = swarm_module.Turtlebot('tb1', 'ROS2_address', [2, 2, 0])
+swarm = swarm_module.Swarm([tb1], [cf1, cf2])
 # print(swarm.crazyflies['cf1'].URI_address)
 
-def get_cf_driver(cf_name):
-    cf_ros2_simulation_pkg = get_package_share_directory('crazyflie_ros2_simulation')
-    robot_description = pathlib.Path(os.path.join(cf_ros2_simulation_pkg, 'resource', 'crazyflie.urdf')).read_text()
-    crazyflie_driver = Node(
-        package = 'webots_ros2_driver',
-        executable = 'driver',
-        namespace = cf_name,
-        output = 'screen',
-        additional_env = {
-            'WEBOTS_CONTROLLER_URL': cf_name,
-        },
-        parameters = [
-            {'robot_description': robot_description},
-        ]
-    )
-    return crazyflie_driver
-
-def get_tb_driver(tb_name):
-    robot_description = pathlib.Path(os.path.join(package_dir, 'resource', 'turtlebot3_burger.urdf')).read_text()
-
 def generate_launch_description():
-    webots_package_dir = get_package_share_directory('webots_pkg')
-
-    webots = WebotsLauncher(
-        world=os.path.join(webots_package_dir, 'worlds', 'swarm_crazyflie_apartment.wbt')
-    )
-    # I don't think the ros2 supervisor can come before the webots launcher. I'm not sure though.
+    package_dir = get_package_share_directory('webots_pkg')
+    robot_description = pathlib.Path(os.path.join(package_dir, 'resource', 'crazyflie.urdf')).read_text() # THIS is for the robot_driver (unnecessary for webots_pkg)
     ros2_supervisor = Ros2SupervisorLauncher()
-    event_handler = launch.actions.RegisterEventHandler(
+    webots = WebotsLauncher(
+        world=os.path.join(package_dir, 'worlds', 'crazyflie_apartment.wbt')
+    )
+    # Robot_state_publisher is a ros2 package that interacts with the Crazyflie urdf to publish the Crazyflie's state
+
+    for cf in swarm.crazyflies:
+        print("cf = ", cf)
+        robot_state_publisher = Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            output='screen',
+            namespace=cf.name,
+            parameters=[{
+                'robot_description': '<robot name=""><link name=""/></robot>'
+            }],
+        )
+    for tb in swarm.turtlebots:
+        robot_state_publisher = Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            output='screen',
+            namespace=tb.name,
+            parameters=[{
+                'robot_description': '<robot name=""><link name=""/></robot>'
+            }],
+        )
+
+
+
+    return LaunchDescription([
+        webots,
+        robot_state_publisher,
+        ros2_supervisor,
+        launch.actions.RegisterEventHandler(
             event_handler=launch.event_handlers.OnProcessExit(
                 target_action=webots,
                 on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
             )
         )
-    launch_description = [webots, ros2_supervisor, event_handler]
-    # The for loop is working, printing the strings correctly
-    for cf in swarm.crazyflies: 
-        launch_description.append(get_cf_driver(cf.name))
-        launch_description.append(Node(
-            package = 'robot_state_publisher',
-            executable = 'robot_state_publisher',
-            additional_env = {'WEBOTS_ROBOT_NAME': cf.name},
-            namespace=cf.name,
-            output = 'screen',
-            parameters = [
-                {'robot_description': '<robot name=""><link name=""/></robot>',}
-                ]
-            ))
-    for tb in swarm.turtlebots:
-        launch_description.append(Node(
-            package = 'robot_state_publisher',
-            executable = 'robot_state_publisher',
-            additional_env = {'WEBOTS_ROBOT_NAME': tb.name},
-            namespace=tb.name,
-            output = 'screen',
-            parameters = [
-                {'robot_description': '<robot name=""><link name=""/></robot>',}
-                ]
-            ))
-
-    return LaunchDescription(launch_description)
-if __name__ == "__main__":
-    generate_launch_description()
+    ])
