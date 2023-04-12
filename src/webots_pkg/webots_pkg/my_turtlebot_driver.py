371c1d81648046de7b75da9641aebc55755ec4e4
Nichols Crawford Taylor <a.nichols.taylor@gmail.com>
2023-04-12 12:05:19 -0600

    Added lidar publishing.
    Added lidar publishing to the turtlebot driver. Publishes a laserscan message to  scan.

--- src/webots_pkg/webots_pkg/my_turtlebot_driver.py
+++ src/webots_pkg/webots_pkg/my_turtlebot_driver.py
@@ -1,5 +1,7 @@
 import rclpy
 from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
+from math import cos, sin, degrees, radians, pi
 
 HALF_DISTANCE_BETWEEN_WHEELS = 0.045
 WHEEL_RADIUS = 0.025
@@ -12,6 +14,11 @@ class MyTurtlebotDriver:
         self._robot_name = self.__robot.getName()
         self.__left_motor = self.__robot.getDevice('left wheel motor')
         self.__right_motor = self.__robot.getDevice('right wheel motor')
+
+        self.lidar = self.__robot.getDevice("LDS-01")
+        self.lidar.enable(self.timestep)
+        self.laser_publisher = self.node.create_publisher(LaserScan, 'scan', 10)
+
 
         self.__left_motor.setPosition(float('inf'))
         self.__left_motor.setVelocity(0)
@@ -43,3 +50,20 @@ class MyTurtlebotDriver:
 
         self.__left_motor.setVelocity(command_motor_left)
         self.__right_motor.setVelocity(command_motor_right)
+
+        self.publish_lidar()
+
+    def publish_lidar(self):
+        ranges = self.lidar.getLayerRangeImage(0)
+        msg = LaserScan()
+        msg.header.stamp = self.__node.get_clock().now().to_msg()
+        msg.header.frame_id = 'base_link_rotated' #THIS IS PROBABLY NOT RIGHT!!!! ACTUALLY A LOT OF THESE MAY BE INACURATE IDK
+        msg.angle_min = -0.5 * pi
+        msg.angle_max = 0.5 * pi
+        msg.angle_increment = pi / (self.lidar.getHorizontalResolution() - 1)
+        #msg.scan_time = self.lidar.getSamplingPeriod() / 1000.0
+        msg.range_min = self.lidar.getMinRange() 
+        msg.range_max = self.lidar.getMaxRange()
+        msg.ranges = ranges
+        self.laser_publisher.publish(msg)
+        #self.__node.get_logger().info(str(ranges))
