dca6474d8509fbb12057f14dbf63ff1e83652dd5
Ryan <riendayves1@gmail.com>
2023-04-19 11:59:08 -0600

    got turtlebots publishing orientation with odometry.
    also have progress on getting location and orientations of all robots

--- src/webots_pkg/webots_pkg/my_turtlebot_driver.py
+++ src/webots_pkg/webots_pkg/my_turtlebot_driver.py
@@ -2,7 +2,8 @@ import rclpy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
-from math import cos, sin, degrees, radians, pi
+from math import cos, sin, degrees, radians, pi, atan2
+import tf2_ros
 
 HALF_DISTANCE_BETWEEN_WHEELS = 0.045
 WHEEL_RADIUS = 0.025
@@ -17,13 +18,14 @@ class MyTurtlebotDriver:
         self.__right_motor = self.__robot.getDevice('right wheel motor')
         self.__gps = self.__robot.getGPS('gps')
         self.__gps.enable(self.timestep)
+        #self.__ort = self.__robot.getQuaternion()
         # self.__gps.enable(self.timestep)
         # self.__imu = self.__robot.getDevice('inertial unit')
         # self.__imu.enable(self.timestep)
         # self.__gyro = self.__robot.getDevice('gyro')
         # self.__gyro.enable(self.timestep)
-        # self.__compass = self.__robot.getDevice('compass')
-        # self.__compass.enable(self.timestep)
+        self.__compass = self.__robot.getDevice('compass')
+        self.__compass.enable(self.timestep)
 
         self.lidar = self.__robot.getLidar("LDS-01")
         self.lidar.enable(self.timestep)
@@ -65,7 +67,7 @@ class MyTurtlebotDriver:
         msg.ranges = ranges
         self.laser_publisher.publish(msg)
         #self.__node.get_logger().info(str(ranges))
-    def _publish_gps(self, gps_vals):
+    def _publish_gps(self, gps_vals,heading):
         # Get position and orientation from the robot
         # position = self.__robot.getPosition()
         # orientation = self.__robot.getOrientation()
@@ -77,6 +79,14 @@ class MyTurtlebotDriver:
         odom.pose.pose.position.x = gps_vals[0]
         odom.pose.pose.position.y = gps_vals[1]
         odom.pose.pose.position.z = gps_vals[2]
+
+        #quaternion = tf2_ros.transformations.quaternion_from_euler(0.0, 0.0, yaw)
+        yaw = atan2(heading[1],heading[0])
+        if self.__namespace =="tb1":
+            #print(self.__compass.getValues())
+            print(yaw*180/pi)
+        odom.pose.pose.orientation.z = sin(yaw / 2.0)
+        odom.pose.pose.orientation.w = cos(yaw / 2.0)
         # odom.pose.pose.orientation.x = orientation[0]
         # odom.pose.pose.orientation.y = orientation[1]
         # odom.pose.pose.orientation.z = orientation[2]
@@ -94,7 +104,8 @@ class MyTurtlebotDriver:
 
         self.__left_motor.setVelocity(command_motor_left)
         self.__right_motor.setVelocity(command_motor_right)
-        # self._publish_gps(self.__gps.getValues())
+        self._publish_gps(self.__gps.getValues(),self.__compass.getValues())#,self.__ort)
+        
         
         # print(gps_values)
         # wasn't working, so I comented this out.
--- src/webots_pkg/webots_pkg/potential_field_node.py
+++ src/webots_pkg/webots_pkg/potential_field_node.py
@@ -1,6 +1,7 @@
 import math
 
 from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
 
 import rclpy
 from rclpy.node import Node
@@ -10,20 +11,79 @@ from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
 
 from turtlesim.srv import Spawn
+import array
+
+from math import atan2
 
 
 class potentialField(Node): # MODIFY NAME
     def __init__(self):
         super().__init__("potential_field") # MODIFY NAME
-        self.cf_frame = self.declare_parameter('cf_frame', 'cf1').get_parameter_value().string_value
-        self.tf_buffer = Buffer()
-        self.tf_listener = TransformListener(self.tf_buffer, self)
+        #self.cf_frame = self.declare_parameter('cf_frame', 'cf1').get_parameter_value().string_value
+        #self.tf_buffer = Buffer()
+        #self.tf_listener = TransformListener(self.tf_buffer, self)
+        self.create_subscription(Odometry,  "tb1/odom", self.tb1_odom_callback, 1)
+        self.create_subscription(Odometry,  "tb2/odom", self.tb2_odom_callback, 1)
+        self.create_subscription(Odometry,  "cf1/odom", self.cf1_odom_callback, 1)
+        
+        #self.tb1_control_publisher = self.tb_driver.create_publisher(Twist, '/tb1/cmd_vel', 10)
+        #print("hello")
+        self.odom_tb1 = Odometry()
+        self.odom_tb2 = Odometry()
+        self.odom_cf1 = Odometry()
+        self.pose_tb1 = [0,0,0]
+        self.pose_tb2 = [0,0,0]
+        self.pose_cf1 = [0,0,0]
+
+    def tb1_odom_callback(self, odom):
+        self.odom_tb1 = odom
+        th = 2 * atan2(odom.pose.pose.orientation.z,odom.pose.pose.orientation.w)
+        self.pose_tb1 = [odom.pose.pose.position.x, odom.pose.pose.position.y, th]
+        
+
+    def tb2_odom_callback(self, odom):
+        self.odom_tb2 = odom
+        th = 2 * atan2(odom.pose.pose.orientation.z,odom.pose.pose.orientation.w)
+        self.pose_tb2 = [odom.pose.pose.position.x, odom.pose.pose.position.y, th]
+    def cf1_odom_callback(self, odom):
+        self.odom_cf1 = odom
+        th = 2 * atan2(odom.pose.pose.orientation.z,odom.pose.pose.orientation.w)
+        self.pose_cf1 = [odom.pose.pose.position.x, odom.pose.pose.position.y, th]
+        #print("got odom cf1")
+
+
+    # def step(self):
+    #     # rclpy.spin_once(self, timeout_sec=0)
+    #     self.tb1_odom_callback()
+    #     print("hello")#self.odom_tb1.pose.pose.position.y)
+    #     #self.get_logger().info('MyPlugin is running')
+    #     # self.get_logger().info('MyPlugin initialized')
+
+    def run(self):
+        while rclpy.ok():
+            #print("running", self.odom_tb1.pose.pose.orientation.z,self.odom_tb1.pose.pose.orientation.w)
+            print(self.pose_tb1)
+            #control turtlebot 1
+            tx_rel = self.pose_tb2[0]-self.pose_tb1[0]
+            ty_rel = self.pose_tb2[1]-self.pose_tb1[1]
+            tx_rel = self.pose_cf1[0]-self.pose_tb1[0]
+            ty_rel = self.pose_cf1[1]-self.pose_tb1[1]
+
+
+
+
+
+
+
+
+            rclpy.spin_once(self)
 
 
 def main(args=None):
     rclpy.init(args=None)
     node = potentialField()
-    rclpy.spin(node)
+    #rclpy.spin(node)
+    node.run()
     rclpy.shutdown()
     
 
