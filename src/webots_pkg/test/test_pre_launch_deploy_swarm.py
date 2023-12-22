4c3a99c869a6d16ad01e42864f5fc3fdacce0929
Will <willgraham345@gmail.com>
2023-12-21 20:28:26 -0700

    Passing tests for generating cf and tb JSON for webot parsing. Next step is to create a swarm and test that in a webots environment.

--- src/webots_pkg/test/test_pre_launch_deploy_swarm.py
+++ src/webots_pkg/test/test_pre_launch_deploy_swarm.py
@@ -1,136 +1,76 @@
 import os
+import os
 import sys
 import pytest
-# insert the path to the webots_pkg directory
+import json
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from webots_pkg.swarm_classes import Swarm
-from worlds.pre_launch_deploy_swarm import WebotsAppender
+from webots_pkg.swarm_classes import cf_dict2json, tb_dict2json
 
-cf_list = []
-tb_list = []
+name = "Robot3"
+translation_unit = [1, 2, 3]
 
+@pytest.mark.pre_launch
+def test_create_cf_dict_json_file():
+    
+    cf = cf_dict2json(name, translation_unit)
 
-@pytest.fixture
-def webots_appender():
-    swarm_instance = Swarm([], [])
+    # cf_robot = cf.cf_dict["Robot"]
+    cf.write_dict_to_json()
 
-    return WebotsAppender(swarm_instance, "test_world.wbt", "new_world.wbt")
+@pytest.mark.pre_launch
+@pytest.mark.dependency(depends=["test_create_cf_dict_json_file"])
+def test_read_cf_dict_json_file():
+    # Remove Robot from the first line 
+    with open('test_cf.json', 'r') as f:
+        first_line = f.readline()
+        rest= f.read()
+        assert first_line == 'Robot {\n'
+        #TODO: Figure out why I can't write "Robot {" to the file
 
-
-def test_generate_tb_string(webots_appender):
-    name = "TurtleBot1"
-    translation_unit = "1 2 3"
-    expected_string = '''
-        TurtleBot3Burger { 
-            translation 1 2 3
-            name "TurtleBot1"
-            controller "<extern>"
-            controllerArgs [
-                ""
-            ]
-            extensionSlot [
-                Solid {
-                    name "imu_link"
-                }
-                GPS {
-                }
-                InertialUnit {
-                    name "inertial_unit"
-                }
-                RobotisLds01 {
-                }
-                Compass {
-                }
-            ]
-        }
-    '''
-    assert webots_appender.generate_tb_string(name, translation_unit) == expected_string
-
-def test_generate_cf_string(webots_appender):
-    name = "Crazyflie1"
-    translation_unit = "4 5 6"
-    expected_string = '''
-        Robot {
-            translation 4 5 6
-            name "Crazyflie1" 
-            children [
-                DEF Multiranger Transform {
-                    translation 0 0 0.02
-                    children [
-                        DistanceSensor {
-                            rotation 0 0 1 -1.57
-                            name "range_right"
-                            lookupTable [
-                                0 0 0
-                                3.5 3500 0
-                            ]
-                        }
-                        DistanceSensor {
-                            rotation 0 0 1 3.14
-                            name "range_back"
-                            lookupTable [
-                                0 0 0
-                                3.5 3500 0
-                            ]
-                        }
-                        DistanceSensor {
-                            rotation 0 0 1 1.57
-                            name "range_left"
-                            lookupTable [
-                                0 0 0
-                                3.5 3500 0
-                            ]
-                        }
-                        DistanceSensor {
-                            name "range_front"
-                            lookupTable [
-                                0 0 0
-                                3.5 3500 0
-                            ]
-                        }
-                    ]
-                }
-                Solid {
-                    translation 0 0 -0.015
-                    children [
-                        DEF battery Shape {
-                            appearance PBRAppearance {
-                                baseColor 0.5 0.5 0.6
-                                metalness 0.1
-                                emissiveIntensity 0
-                            }
-                            geometry Mesh {
-                                url [
-                                    "meshes/battery.stl"
-                                ]
-                            }
-                        }
-                    ]
-                }
-            ]
-        }
-    '''
-    assert webots_appender.generate_cf_string(name, translation_unit) == expected_string
-
-# Add more tests for other methods in the WebotsAppender class
+    with open('test_cf.json', 'w') as f:
+        f.write("{")
+        f.write(rest)
+    
+    with open('test_cf.json', 'r') as f:
+        cf_robot = json.load(f)
+        print(cf_robot)
 
 
-# def test_append_to_world(webots_appender):
-#     # Create a mock world file for testing
-#     world_filepath = ...  # Provide the path to a test world file
-#     new_world_filename = ...  # Provide a new filename for the updated world file
-#     webots_appender.world_filepath = world_filepath
-#     webots_appender.new_world_filename = new_world_filename
+    assert cf_robot["name"] == name
+    assert cf_robot["translation"] == translation_unit
+    assert cf_robot["children"][0]["DEF"] == "Multiranger"  
 
-#     # Call the append_to_world method
-#     webots_appender.append_to_world()
+    # os.remove('test_cf.json')
 
-#     # Assert that the new world file is created
-#     assert os.path.exists(new_world_filename)
+@pytest.mark.pre_launch
+def test_create_tb_dict_json_file():
+    tb = tb_dict2json(name, translation_unit)
 
-#     # Assert that the content of the new world file is correct
-#     with open(new_world_filename, 'r') as file:
-#         new_world_data = file.read()
-#     assert new_world_data == ...
+    # tb_robot = tb.tb_dict["Turtlebot3Burger"]
+    tb.write_dict_to_json()
+    assert 1 == 1
 
-# # Add more tests for other scenarios in the append_to_world method
+@pytest.mark.pre_launch
+@pytest.mark.dependency(depends=["test_create_tb_dict_json_file"])
+def test_read_tb_dict_json_file():
+    # Remove Robot from the first line 
+    with open('test_tb.json', 'r') as f:
+        first_line = f.readline()
+    
+    with open('test_tb.json', 'r') as f:
+        rest= f.read()
+    
+    with open('test_tb.json', 'w') as f:
+        f.write("{")
+        f.write(rest)
+    assert first_line == 'TurtleBot3Burger {\n'
+
+    os.remove('test_tb.json')
+    
+
+    
+
+
+if __name__ == "__main__":
+    # if __name__ == '__main__': run pytest
+    pytest.main([__file__])
