5a678195cd3edb4f80301e598c96fe117d582b44
Will <willgraham345@gmail.com>
2023-12-29 14:41:48 -0700

    Fixed writing LHMem test. Now passing

--- src/webots_pkg/test/test_pre_launch_cf_lighthouse_configuration.py
+++ src/webots_pkg/test/test_pre_launch_cf_lighthouse_configuration.py
@@ -19,36 +19,50 @@ def test_ReadLHMem(radio_uri = "radio://0/80/2M/E7E7E7E7E7"):
     # Initialize the low-level drivers
     cflib.crtp.init_drivers()
 
+    print()
+    print('------------------')
+    print("INITIAL MEMORY")
+    print()
     temp_LHMem = ReadLHMem(radio_uri)
     assert temp_LHMem.geo_data[0] is not None
     assert temp_LHMem.calib_data[0] is not None
 
 
-def test_WriteLHMem(uri = "radio://0/80/2M/E7E7E7E7E7"):
+def test_WriteLHMem(radio_uri = "radio://0/80/2M/E7E7E7E7E7"):    # 
+    uri = uri_helper.uri_from_env(radio_uri)
 
-    # Read the current values
-    # temp_memory = ReadLHMem(uri)
-
+    # Initialize the low-level drivers
+    cflib.crtp.init_drivers()
     # Create a LighthouseBsGeometry object
     bs1geo = LighthouseBsGeometry()
-
-
-    # Modify the origin and rotation_matrix fields
-    bs1geo.origin = [0.0, 0.0, 0.1] # Best guess on wheere the base station is on tb
+    bs1geo.origin = [0.0, 0.0, 1.0] # Best guess on wheere the base station is on tb
     bs1geo.rotation_matrix = [
         [0.0, -1.0, 0.0],
         [1.0, 0.0, 0.0],
-        [0.0, 0.0, 1.0],
+        [0.0, 0.0, 7.0],
     ]
+    bs1geo.valid = True
+
+    # Read the current values
+    print()
+    print('------------------')
+    print("INITIAL MEMORY FOR WRITING LH CONFIG")
+    temp_cf_lh_memory = ReadLHMem(radio_uri)
+    print()
+    calib_data = temp_cf_lh_memory.calib_data[0]
 
     # Write geo memory to crazyflie
     geo_dict = {0: bs1geo}
-    WriteLHGeoMem(uri, geo_dict)
+    calib_dict = {0: calib_data}
+    WriteLHGeoMem(radio_uri, geo_dict)
 
     # Read new values
-    new_memory = ReadLHMem(uri)
-    #FIXME: Test this with a crazyflie that isn't throwing a fit. 
-    assert 1 == 1
+    print()
+    print('------------------')
+    print("NEW MEMORY AFTER WRITING LH CONFIG")
+    print()
+    new_memory = ReadLHMem(radio_uri)
+    assert new_memory.geo_data[0].origin == bs1geo.origin
 
 
 
@@ -70,6 +84,6 @@ def test_WriteLHMem(uri = "radio://0/80/2M/E7E7E7E7E7"):
 #     cf.close_link()
 
 if __name__ == "__main__":
-    test_ReadLHMem()
-    # test_WriteLHMem()
+    # test_ReadLHMem()
+    test_WriteLHMem()
     # test_cf_lh_config()
